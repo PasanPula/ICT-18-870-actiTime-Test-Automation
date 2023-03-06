354f4da2e85337323b71315aa6815c90aa689e7d
PasanPula <ppulasithi@gmail.com>
2023-03-06 11:50:13 +0530

    Add cucumber test reports

--- src/main/java/com/actitime/qa/testrunner/TestRunner.java
+++ src/main/java/com/actitime/qa/testrunner/TestRunner.java
@@ -8,6 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "/Users/Pasan/Desktop/ICT_18_870/HRTestAutomation/src/main/java/com/actitime/qa/feature/NewUserCreate.feature", 
 		glue = {"com/actitime/qa/stepdefinitions" }, 
+		plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+				   "junit:target/cucumber-reports/Cucumber.xml",
+				   "html:target/cucumber-reports/cucumber-report.html"}, //to generate different types of reporting
 		monochrome = true, 
 		strict = true, 
 		dryRun = false, 
